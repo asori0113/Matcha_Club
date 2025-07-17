6c5cf0ca91a7426f9d817263a99f83f2b0c5c684
Aljhay Soriano <147592801+asori0113@users.noreply.github.com>
2025-07-17 15:42:35 -0700

    Added arcjet bot protection and token refills

--- website/server-2/app.js
+++ website/server-2/app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongoDB.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use(arcjetMiddleware);
 
 app.use(errorMiddleware);
 
